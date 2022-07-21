aaa4c30c7d86cd17fdea9677f0f476f91deab571
Mohammed Abdelkawy <engineer_m.abdelkawy@yahoo.com>
2022-07-21 05:29:08 -0500

    add owner index page and controller, closes #13

--- sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -1,14 +1,11 @@
 package guru.springframework.sfgpetclinic.controllers;
 
-import guru.springframework.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
-
-    private VetService vetService;
 
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
     public String listVets(Model model){
