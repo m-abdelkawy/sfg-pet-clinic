bd678a6c915228af235fae9327e0e63e16b843c8
Mohammed Abdelkawy <engineer_m.abdelkawy@yahoo.com>
2022-07-21 05:16:34 -0500

    add vet index page and controller, closes #7

---
+++ sfg-pet-clinic/pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetController.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.controllers;
+
+import guru.springframework.sfgpetclinic.services.VetService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    private VetService vetService;
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listVets(Model model){
+        return "vets/index";
+    }
+}
