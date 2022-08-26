1d9f50243c62e7886555533313fbb736c2bf5a6b
Mohammed Abdelkawy <engineer_m.abdelkawy@yahoo.com>
2022-08-25 21:36:15 -0500

    circleci setup

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Specialty.java
@@ -0,0 +1,17 @@
+package guru.springframework.sfgpetclinic.model;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "specialties")
+public class Specialty extends BaseEntity{
+    private String description;
+}
